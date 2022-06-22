b6a1dbd092d65d4c94e591a256bc4c3479dbc031
greenpearbear <romadmitr12@gmail.com>
2022-06-22 22:07:45 +0700

    настройка Python-Dotenv

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -96,11 +96,11 @@ DJOSER = {
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
