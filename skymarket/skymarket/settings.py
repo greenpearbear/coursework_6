689c6658825dafba55167fc35123853da8dcf1a5
greenpearbear <romadmitr12@gmail.com>
2022-06-27 21:05:06 +0700

    правки отправки почты

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -176,6 +176,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_USER = '.env.example@gmail.com'
 EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
