1a9d43503321246dad65b27acf871a64192364ca
greenpearbear <romadmitr12@gmail.com>
2022-06-27 22:10:24 +0700

    начал делать модели объявлений и комментариев

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=30)
+    price = models.IntegerField()
+    description = models.CharField(max_length=254)
+    author
 
 
 class Comment(models.Model):
